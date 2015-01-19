966debdd735e3430c84bed68d09d5ffcbd3370db
Daniel Garijo <dgarijo@fi.upm.es>
2015-01-19 19:08:37 +0100

    Adapted the mapper to the newer version of Wings.
    Serializations now updated for OPMW and PROV.
    Also, I have added the rationale of the mapping and our requirements.
    Added new test data.

--- AWINGS_OPM_Mapper/src/opmw_mapper/deprecated/Tests.java
+++ AWINGS_OPM_Mapper/src/opmw_mapper/deprecated/Tests.java
@@ -1,12 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2012-2013 Ontology Engineering Group, Universidad Politécnica de Madrid, Spain
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
-package wings_opm_mapper;
+package opmw_mapper.deprecated;
+
+import opmw_mapper.Mapper;
 
 /**
- *
+ * This class has to be replaced with a test
  * @author Daniel Garijo
  */
 public class Tests {
@@ -16,9 +29,21 @@ public class Tests {
 //        if(m.isRunPublished("http://wind.isi.edu:8890/sparql", "http://wind.isi.edu/marbles/assets/components/workflow_portal/users/1/wordbags/runs/run_130.ttl")) System.out.println("The run exists!!");
 //        else System.out.println("The run does not exist");
         
-        //Template transformation example
-        m.transformWINGSElaboratedTemplateToOPM("src\\SampleData\\LongRunningID3Modeler.owl","RDF/XML", "LongRunningID3Modeler.ttl");
-        m.transformWINGSElaboratedTemplateToOPM("src\\SampleData\\Structured_Association_Test.owl","RDF/XML", "StructuredAssociationTest.ttl");
+        //new tests without collections, but abstract components, etc.
+        String s = m.transformWINGSElaboratedTemplateToOPMW("src\\sample_data\\new\\abs_words\\Template.owl","RDF/XML", "newExample.ttl");
+        String s1 = m.transformWINGSResultsToOPMW("src\\sample_data\\new\\abs_words\\Execution.owl", "src\\sample_data\\new\\abs_words\\Library.owl","RDF/XML", "reusltsOPMW.ttl", "resultsPROV.ttl");
+        // new tests with collections
+//        m.transformWINGSElaboratedTemplateToOPMW("src\\sample_data\\new\\aquaflow_ntm\\Template.owl","RDF/XML", "newExample.ttl");
+//        m.transformWINGSResultsToOPMW("src\\sample_data\\new\\aquaflow_ntm\\Execution.owl", "src\\sample_data\\new\\aquaflow_ntm\\Library.owl","RDF/XML", "reusltsOPMW.ttl", "resultsPROV.ttl");
+        System.out.println("Template: "+s+"\n"+"Execution: "+s1);
+
+
+
+
+
+
+//old tests        
+//        m.transformWINGSElaboratedTemplateToOPM("src\\SampleData\\Structured_Association_Test.owl","RDF/XML", "StructuredAssociationTest.ttl");
         //m.transformWINGSElaboratedTemplateToOPM("src\\SampleData\\AbstractGlobalWorkflow2.owl","RDF/XML", "globalWF2.ttl");
 //        m.transformWINGSElaboratedTemplateToOPM("src\\SampleData\\AbstractSubWFLigandBindingSitesComparison.owl","RDF/XML", "ligandComparisonOPM.ttl");
 //        m.transformWINGSElaboratedTemplateToOPM("src\\SampleData\\AbstractSubWfDocking.owl","RDF/XML", "dockingOPM.ttl");
--- README.md
+++ README.md
@@ -4,11 +4,37 @@ WingsProvenanceExport
 Provenance export for the Wings workflow system, using the OPMW vocabulary. 
 The current serializations supported are:
 
-* OPM: OPMW is an OPM Profile for scientific workflow publication. Official website: http://opmw.org
+* OPMW: OPMW is an OPM Profile for scientific workflow publication. Official website: http://www.opmw.org/ontology/
 
-* PROV: OPMW extends PROV and it is fully compatible. More information in :http://www.opmw.org/node/8
+* PROV: OPMW extends PROV and it is fully compatible. More information in : http://www.opmw.org/model/OPMW/#ontDescription
 
-Input: owl ontologies and ttl files used to specify templates and summary of traces, respectively.
-Output: 2 files: one in PROV and one in OPMW. The names can be specified.
+OPMW also extends the P-Plan ontology (http://purl.org/net/p-plan). Although a P-Plan serialization is not currently exported, 
+OPMW is fully aligned as an extension of P-Plan. More information in http://www.opmw.org/model/OPMW/#ontDescription.
+
+Inputs and outputs
+=====================
+The conversion of templates and executions is independent. Therefore, there are two main methods to obtain the OPMW and PROV serializations:
+
+Templates: 
+
+ public String transformWINGSElaboratedTemplateToOPMW(String template,String mode, String outFile)
+ 
+* Input: the path of the .owl file of the Wings template, the serialization in which it's written (e.g., "RDF/XML") and the path of the file where you want the result.
+
+* Output: The URI assigned to the workflow template.
+
+Executions: 
+
+ public String transformWINGSResultsToOPMW(String resultFile, String libraryFile, String modeFile, String outFilenameOPMW, String outFilenamePROV)
+ 
+* Input: the path of the .owl file of the Wings template, the path of the .owl file with the Wings execution library, the serialization in which both files are written (e.g., "RDF/XML") and the path of the files where you want the results (OPMW and PROV serializations are written in two files, but they share the same URIs).
+
+* Output: The URI assigned to the workflow execution account.
 
 The project is based on the Jena framework for the conversion.
+
+Assumptions
+=====================
+In order to link executions and templates correctly, the mapper assumes that variables and steps have asserted the relationship "wflow:derivedFrom", which states from which node in the original template comes the variable or step. Therefore, older versions of Wings are not supported. 
+
+Acknowledgements: I would like to thank Varun Ratnakar for his support while doing the mapper.
